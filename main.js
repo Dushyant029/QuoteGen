e55ae3f44607a2f99bed9bd3d9145426e5e948ce
K. Dushyant Reddy <55031190+Dushyant029@users.noreply.github.com>
2021-01-19 22:22:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ $.ajax({
     $(document).ready(function(){
         $("#a").text(res.content);
         $("#b").text("- "+res.author);
-        $("#fb-share").attr('href','https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2FDushyant029.github.io%2Fquotegen&quote='+res.content+'%0A-%20'+res.author+'%0A%0ADushyant029.github.io%2Fquotegen%2F');
-        $("#tweet").attr('href','https://twitter.com/intent/tweet?source=http%3A%2F%2FDushyant029.github.io%2Fquotegen&text='+res.content+'%0A-%20'+res.author+'%0A%0ADushyant029.github.io%2Fquotegen%2F');
+        $("#fb-share").attr('href','https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2FDushyant029.github.io%2FQuoteGen&quote='+res.content+'%0A-%20'+res.author+'%0A%0ADushyant029.github.io%2FQuoteGen%2F');
+        $("#tweet").attr('href','https://twitter.com/intent/tweet?source=http%3A%2F%2FDushyant029.github.io%2FQuoteGen&text='+res.content+'%0A-%20'+res.author+'%0A%0ADushyant029.github.io%2FQuoteGen%2F');
     });
   }
 });
